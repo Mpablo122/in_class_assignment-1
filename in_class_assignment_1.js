e8a0bbb5420ef3e813f9102570c4a753680e25fd
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-01-30 16:17:38 -0500

    completed task 2

--- in_class_assignment_1.js
+++ in_class_assignment_1.js
@@ -1,2 +1,18 @@
 // Task 1 
+let expenses = 10000
+if (expenses > 7000) {
+    console.log("High expenses");
+} else { 
+    console.log("Manageable Expenses");
+    
+}
 
+//Task 2 
+
+let student_score = 80;
+
+if (student_score >= 60) {
+    console.log("Passed");
+} else { 
+    console.log("Failed");
+}
